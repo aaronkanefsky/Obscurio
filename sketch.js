20a0d594f63aef439cebe39cc36b2c36d13cffd3
aaronkanefsky <aaronk03@vt.edu>
2025-10-26 14:50:06 -0400

    Added Fonts

--- sketch.js
+++ sketch.js
@@ -1,3 +1,13 @@
+/**
+ * Obscurio
+ * 
+ * By: Emma Wallace and Aaron Kanefsky 
+ */
+let myFont;
+function preload(){
+  myFont = loadFont('/assets/fonts/Firlest-Regular.otf');
+}
+
 function setup() {
   createCanvas(500, 500);
   frameRate(60);
@@ -5,6 +15,11 @@ function setup() {
 
 function draw() {
   background(255, 0, 0);
+
+  textFont(myFont);
+  textSize(40)
+  text("The quick brown fox jumped", 10, 200)
+  text("over the lazy dog!", 30, 235)
 }
 
 
