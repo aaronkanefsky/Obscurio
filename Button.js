09f1e1c085d638a6eb73740cfab4bfc53856b604
aaronkanefsky <aaronk03@vt.edu>
2025-10-27 13:37:35 -0400

    Button Hover

--- Button.js
+++ Button.js
@@ -1,40 +1,53 @@
-const ButtonStates ={
-    UP: 'up',
-    CLICKED: 'clicked',
-    RELEASED: 'released',
-}
+const ButtonStates = {
+  UP: 'up',
+  CLICKED: 'clicked',
+  RELEASED: 'released',
+};
 
-class Button{
-    constructor(x, y, height, width, bodyText, size, font, backgroundColor){
-        this.x = x;
-        this.y = y;
-        this.height = height;
-        this.width = width;
-        this.bodyText = bodyText;
-        this.size = size;
-        this.font = font;
-        this.backgroundColor = backgroundColor;
-        this.clickedState = ButtonStates.UP;
+class Button {
+  constructor(x, y, width, height, bodyText, size, font, backgroundColor) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.bodyText = bodyText;
+    this.size = size;
+    this.font = font;
+    this.backgroundColor = backgroundColor;
+    this.clickedState = ButtonStates.UP;
+  }
+
+  isHovered() {
+    // Check if mouse is inside the button boundaries
+    return (
+      mouseX > this.x - this.width / 2 &&
+      mouseX < this.x + this.width / 2 &&
+      mouseY > this.y - this.height / 2 &&
+      mouseY < this.y + this.height / 2
+    );
+  }
+
+  drawButton() {
+    push();
+    rectMode(CENTER);
+    stroke(0);
+    strokeWeight(3);
+    fill(this.backgroundColor);
+    rect(this.x, this.y, this.width, this.height);
+
+    fill(0);
+    noStroke();
+    textSize(this.size);
+    textFont(this.font);
+    textAlign(CENTER, CENTER);
+    text(this.bodyText, this.x, this.y);
+    pop();
+
+    // Change cursor if hovered
+    if (this.isHovered()) {
+      cursor('pointer');
+    } else {
+      cursor('default');
     }
-
-    drawButton(){
-        push();
-
-        // Button
-        rectMode(CENTER);
-        stroke(0);
-        strokeWeight(3);
-        fill(this.backgroundColor);
-        rect(this.x, this.y, this.height, this.width);
-
-        // Text in the button
-        fill(0)
-        strokeWeight(0);
-        textSize(this.size)
-        textFont(this.font)
-        textAlign(CENTER, CENTER);
-        text(this.bodyText, this.x, this.y)
-        pop();
-    }
-
+  }
 }
