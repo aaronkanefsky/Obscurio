a01be868977c0a5e1eba4d91022cc9a054c55510
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-11-10 11:47:18 -0500

    Create folioObj.js

---
+++ folioObj.js
@@ -0,0 +1,12 @@
+/**
+ * @class folioObj
+ * 
+ * @description Object class to contain doors stored in folio for traitor to pick trick doors from
+ */
+class folioObj {
+  constructor(x,y,d) {
+    this.x = x;
+    this.y = y;
+    this.door = d;
+  }
+}
