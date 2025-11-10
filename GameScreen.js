dd55f3bf22c361ac666996fbcf4e3546ba933823
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-11-10 11:38:54 -0500

    Update GameScreen.js to handle game loop screen states

--- GameScreen.js
+++ GameScreen.js
@@ -4,16 +4,68 @@ class GameScreen {
     this.gameDoors = shuffle(this.doors);
     this.exit = null;
     this.level = 1;
-    this.states = [new GrimoireCluesScreen(), new TraitorPickScreen(), new ShowGrimoireScreen(), new pickExitScreen()];
-    this.gameState = grimoireCluesScreen;
+    this.loopStates = [this.grimoireCluesScreen = new GrimoireCluesScreen(game), this.traitorPickScreen = new TraitorPickScreen(game), this.exitSelectScreen = new ExitSelectScreen(game)];
+    this.gameLoopState = this.grimoireCluesScreen;
   }
-
-  changeState(x) {
-    this.gameState = x;
-  }
-
+  
   enter() {
     this.exit = this.gameDoors.pop();
     this.levelDoors.push(new doorObj(true,this.exit));
+  }
+
+  draw() {
+    background(220);
+    if(this.gameLoopState === this.loopStates.GRIMOIRE_CLUES_SCREEN) {
+      handleGrimoireCluesScreen();
+    }
+    else if(this.gameLoopState === this.loopStates.TRAITOR_PICK_SCREEN) {
+      handleTraitorPickScreen();
+    }
+    else {
+      handleExitSelectScreen();
+    }
+  }
+
+  handleGrimoireCluesScreen() {
+    this.grimoireCluesScreen.update();
+    this.grimoireCluesScreen.draw();
+
+    if(this.grimoireCluesScreen.nextButton.released === true) {
+      this.gameLoopState = this.loopStates.TRAITOR_PICK_SCREEN;
+      this.grimoireCluesScreen.exit();
+      this.traitorPickScreen.enter();
+    }
+  }
+
+  handleTraitorPickScreen() {
+    this.traitorPickScreen.update();
+    this.traitorPickScreen.draw();
+
+    if(this.traitorPickScreen.nextButton.released === true) {
+      this.gameLoopState = this.loopStates.EXIT_SELECT_SCREEN;
+      this.traitorPickScreen.exit();
+      this.exitSelectScreen.enter();
+    }
+  }
+
+  handleExitSelectScreen() {
+    this.exitSelectScreen.update();
+    this.exitSelectScreen.draw();
+
+    if(this.exitSelectScreen.nextButton.released === true) {
+      this.gameLoopState = this.loopStates.GRIMOIRE_CLUES_SCREEN;
+      this.exitSelectScreen.exit();
+      if(this.level > 6) {
+        this.game.gameState = this.game.gameStates.WIN_SCREEN;
+        this.game.winScreen.enter();
+      }
+      else if(this.cohesionTokens <= 0) {
+        this.game.gameState = this.game.gameStates.LOSE_SCREEN;
+        this.game.loseScreen.enter();
+      }
+      else {
+        this.grimoireCluesScreen.enter();
+      }
+    }
   }
 }
