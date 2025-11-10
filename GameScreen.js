03270457360fb1df070eea7ebd5eeda2241bb677
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-11-09 19:22:41 -0500

    Update GameScreen.js

--- GameScreen.js
+++ GameScreen.js
@@ -4,11 +4,16 @@ class GameScreen {
     this.gameDoors = shuffle(this.doors);
     this.exit = null;
     this.level = 1;
-    this.states = [grimoireCluesScreen = new GrimoireCluesScreen(), traitorPickScreen = new TraitorPickScreen(), showGrimoireScreen = new ShowGrimoireScreen(), pickExitScreen = new pickExitScreen()];
+    this.states = [new GrimoireCluesScreen(), new TraitorPickScreen(), new ShowGrimoireScreen(), new pickExitScreen()];
     this.gameState = grimoireCluesScreen;
   }
 
   changeState(x) {
     this.gameState = x;
+  }
+
+  enter() {
+    this.exit = this.gameDoors.pop();
+    this.levelDoors.push(new doorObj(true,this.exit));
   }
 }
