c8993854ab4013c8bdda01a8b8623e64b42dfb09
Emma Wallace <emmaw21@vt.edu>
2025-11-12 11:38:19 -0500

    added comments to doorObj

--- src/Actors/doorObj.js
+++ src/Actors/doorObj.js
@@ -1,8 +1,8 @@
 class doorObj {
   constructor(i,e,d) {
     this.index = i;
-    this.count = 0;
-    this.exitCard = e;
-    this.door = d;
+    this.count = 0;     // count of how many times this door has been picked this level
+    this.exitCard = e;  // marker that this door is the exit for the current level
+    this.door = d;      // image of door
   }
 }
