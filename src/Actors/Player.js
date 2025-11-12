c9862116b50a451b8da74623be1a0d17bd47e189
Emma Wallace <emmaw21@vt.edu>
2025-11-11 20:26:40 -0500

    added button to view grimoire clues

--- src/Actors/Player.js
+++ src/Actors/Player.js
@@ -3,5 +3,14 @@ class Player {
     this.playerId = player_num;
     this.character = char_num;
     this.playerType = null;
+    this.position = new p5.Vector(285,570);
+  }
+
+  updatePlayer() {
+
+  }
+
+  drawPlayer() {
+    this.character.walk();
   }
 }
