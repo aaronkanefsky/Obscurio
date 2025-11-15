ec25ef475d77abebf61395001726fdee38144f72
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-11-14 20:34:02 -0500

    Update Player.js

--- src/Actors/Player.js
+++ src/Actors/Player.js
@@ -5,13 +5,58 @@ class Player {
     this.character = char_num;
     this.playerType = null;
     this.position = new p5.Vector(0,0);
+    this.direction = 2;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.moveFrameCount = 0;
   }
 
+  walk() {
+    if(this.moveFrameCount % 6 === 0) {
+      this.frameX = (this.frameX + 1) % 8;
+    }
+    
+    this.frameY = this.direction;
+
+    let charWalkWidth = 512/8;
+    let charWalkHeight = 256/4;
+    let x_pos = this.frameX * charWalkWidth;
+    let y_pos = this.frameY * charWalkHeight;
+
+    let walkFrame = charWalk[this.character].get(x_pos, y_pos, charWalkWidth, charWalkHeight);
+
+    image(walkFrame,this.position.x,this.position.y,60,60);
+
+    this.moveFrameCount++;
+  }
+  
+  spell() {
+    if(this.moveFrameCount % 6 === 0) {
+      this.frameX = (this.frameX + 1) % 7;
+    }
+    
+    this.frameY = this.direction;
+
+    let charSpellWidth = 448/7;
+    let charSpellHeight = 256/4;
+    let x_pos = this.frameX * charSpellWidth;
+    let y_pos = this.frameY * charSpellHeight;
+
+    let spellFrame = charSpell[this.character].get(x_pos, y_pos, charSpellWidth, charSpellHeight);
+
+    image(spellFrame,this.x,this.y,60,60);
+
+    this.moveFrameCount++;
+  }
+  
   updatePlayer() {
 
   }
 
   drawPlayer() {
-    characters[this.character].walk(this.position.x,this.position.y);
+    push();
+    translate(this.position.x,this.position.y);
+    this.walk(0,0);
+    pop();
   }
 }
